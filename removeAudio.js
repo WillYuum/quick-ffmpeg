5a21a76e30253664d6c4983701867b34c9a86d99
WillYum <liamshere98@gmail.com>
2024-03-13 22:36:18 +0200

    Implement getSaveDir to remove audio

--- removeAudio.js
+++ removeAudio.js
@@ -1,27 +1,38 @@
 const ffmpegPath = require('ffmpeg-static');
 const ffmpeg = require('fluent-ffmpeg');
-const getFileDirectory = require('./system_dialog_prompter');
+const { getFileDirectory, getSaveDir } = require('./system_dialog_prompter');
+const path = require('path');
 
 // const inputFilePath = './input/video_with_audio.mp4'; // Replace with your input file path
 getFileDirectory((inputFilePath) => {
     const outputFilePath = './output/video_without_audio.mp4'; // Replace with your output file path
 
-    handleRemovingAudio(inputFilePath, outputFilePath);
+    handleRemovingAudio(inputFilePath);
 });
 
-function handleRemovingAudio(inputFilePath, outputFilePath) {
+function handleRemovingAudio(inputFilePath) {
     const ffmpegProcess = ffmpeg(inputFilePath)
         .setFfmpegPath(ffmpegPath)
         .noAudio(); // Remove audio
 
-    ffmpegProcess
-        .on('end', function () {
-            console.log('Audio removal finished.');
-        })
-        .on('error', function (err) {
-            console.log('Error: ' + err);
-        })
-        .output(outputFilePath)
-        .videoCodec('copy')
-        .run();
+
+    getSaveDir((outputFilePath) => {
+
+        const outputFileName = path.basename(outputFilePath);
+        const hasExtension = outputFileName.includes('.mp4');
+        const updatedOutputFilePath = hasExtension ? outputFilePath : `${outputFilePath}.mp4`;
+
+
+        ffmpegProcess
+            .on('end', function () {
+                console.log('Audio removal finished.');
+
+            })
+            .on('error', function (err) {
+                console.log('Error: ' + err);
+            })
+            .output(updatedOutputFilePath)
+            .videoCodec('copy')
+            .run();
+    });
 }
