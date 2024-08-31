ffc2a4666ab28c91ef1eae5d89fcde0c083be0af
WillYum <liamshere98@gmail.com>
2024-08-31 21:18:29 +0300

    Add support for selecting multiple files

--- system_dialog_prompter.js
+++ system_dialog_prompter.js
@@ -21,6 +21,20 @@ function getFileDirectory(cb) {
 
 }
 
+function getMultipleFiles(cb) {
+    const config = { type: DIALOG_TYPES.open_multiple_files };
+
+    dialog(config)
+        .then(files => {
+            //remove /r from the end of the string
+            files = files.map(file => file.replace(/\r$/, ''));
+
+
+            cb(files);
+        })
+        .catch(err => console.log(GetErrorReasonMessage(err)));
+}
+
 function getSaveDir(cb) {
     const config = { type: DIALOG_TYPES.save_file };
 
@@ -44,4 +58,4 @@ function GetErrorReasonMessage(err) {
     }
 }
 
-module.exports = { getFileDirectory, getSaveDir };
+module.exports = { getFileDirectory, getSaveDir, getMultipleFiles };
